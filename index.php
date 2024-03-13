bd5a033dc1df9eb144ed2097dcb4af6fabafe8e4
prashantritgit <111300640+prashantritgit@users.noreply.github.com>
2024-03-13 16:29:01 +0530

    Update index.php again

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Sample</title>
+</head>
+<body>
+
 <?php
+// PHP code starts here
+echo "<h1>Hello, World!</h1>";
+echo "<p>This is a sample PHP code.</p>";
+// PHP code ends here
+?>
 
-print "Hello World"
-
-?> 
+</body>
+</html>
